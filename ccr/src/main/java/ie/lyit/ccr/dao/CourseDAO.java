2c53ccb274c5ab66a257d2a52e32a9ad3f0a0455
L00162875 <78763031+L00162875@users.noreply.github.com>
2021-08-03 19:48:13 +0100

    Dao updates (#21)
    * Remove target dir from source control
    
    * Adding some sample db access

--- ccr/src/main/java/ie/lyit/ccr/dao/CourseDAO.java
+++ ccr/src/main/java/ie/lyit/ccr/dao/CourseDAO.java
@@ -1,12 +1,15 @@
 package ie.lyit.ccr.dao;
 
+import java.io.Serializable;
 import java.util.List;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import ie.lyit.ccr.model.entities.Course;
 import ie.lyit.ccr.util.HibernateUtil;
-import ie.lyit.ccr.util.PetHelpDataStoreSingleton;
+
+import javax.persistence.EntityManager;
 
 /**
  *
@@ -14,63 +17,54 @@ import ie.lyit.ccr.util.PetHelpDataStoreSingleton;
  */
 public class CourseDAO {
 
-	private static PetHelpDataStoreSingleton singleton = PetHelpDataStoreSingleton.getInstance();
-	private SessionFactory sessionFactory = null;
+	private SessionFactory sessionFactory;
+	private EntityManager entityManager;
 
 	public CourseDAO() {
 		sessionFactory = HibernateUtil.getSessionFactory();
 	}
 
 	public boolean updateCourse(Course toUpdate) {
-		if (toUpdate != null) {
-			boolean wasUpdated = this.updateCourse(toUpdate);
-			if (wasUpdated) {
-				return true;
-			} else {
-				return false;
-			}
+		if (toUpdate == null) {
+			return false;
 		}
-		return false;
+		entityManager.getTransaction().begin();
+		entityManager.merge(toUpdate);
+		entityManager.getTransaction().commit();
+		return true;
 	}
 
 	public boolean createCourse(Course newCourse) {
-		if (newCourse != null) {
-
-			return true;
+		if (newCourse == null) {
+			return false;
 		}
-		return false;
+		Session session = sessionFactory.getCurrentSession();
+		Serializable id = session.save(newCourse);
+		if (id == null) {
+			return false;
+		}
+		return true;
 	}
 
 	public boolean deleteCourse(Course toDelete) {
-		if (toDelete != null) {
-			return true;
-		}
-		return false;
+		throw new RuntimeException("not implemented");
 	}
 
 	public Course findCourse(Course toFind) {
-		if (toFind != null) {
-			return null;
-		}
-		return null;
+		throw new RuntimeException("not implemented");
 	}
 
-	public Course findCourseById(String id) {
-		if (id != null) {
-			return null;
-		}
-		return null;
+	public Course findCourseById(Long id) {
+		return entityManager.find(Course.class, new Long(id));
 	}
 
 	public List<Course> findAllCourses() {
-		return null;
+		List<Course> courses = entityManager.createQuery("SELECT * from courses")
+				.getResultList();
+		return courses;
 	}
 
 	public List<Course> findMyOwnCourses(String userName) {
-
-		if (userName != null) {
-			return null;
-		}
-		return null;
+		throw new RuntimeException("not implemented");
 	}
 }
